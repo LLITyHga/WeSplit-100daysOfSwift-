40893df2a8b0d486aac15e8cd964074cd4c78773
Андрій Пікусь <llityhga@gmail.com>
2024-06-18 08:30:24 +0300

    Initial Commit

---
+++ WeSplit(100daysOfSwift)/WeSplit_100daysOfSwift_App.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplit_100daysOfSwift_App.swift
+//  WeSplit(100daysOfSwift)
+//
+//  Created by Wolf on 18.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplit_100daysOfSwift_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
